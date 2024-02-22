7673e9f8dc920b7ae39c349f49610d31e3e69d35
Kevin Machava <72518473+Keviin725@users.noreply.github.com>
2024-02-22 16:50:31 +0200

    save

--- app.js
+++ app.js
@@ -11,3 +11,28 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+/**window.onload = function() {
+    const icon = document.querySelector('.software-detail');
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+  
+    icon.style.position = 'relative';
+    icon.style.left = Math.random() * windowWidth + 'px';
+    icon.style.top = Math.random() * windowHeight + 'px';
+  };
+
+  function moveIcons() {
+    const icons = document.querySelectorAll('.software-detail');
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+  
+    icons.forEach(icon => {
+      icon.style.position = 'relative';
+      icon.style.left = Math.random() * windowWidth + 'px';
+      icon.style.top = Math.random() * windowHeight + 'px';
+    });
+  }
+  
+  // Move os ícones para novas posições a cada 5 segundos
+  setInterval(moveIcons, 5000); */
